fde01b70bd77cb43384355a65010ced8fdd12075
RP <rp@g.com>
2020-04-11 14:54:14 +0530

    added dropdown for year

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,14 @@
         <input type="text" v-model="mov.name" placeholder="Enter Movie Name" />
         <br />Category:
         <input type="text" v-model="mov.category" placeholder="Enter Category" />
-        <br />Release Year:
-        <input type="text" v-model="mov.year" placeholder="Enter Release year" />&nbsp;
+        <br />Release Year:<select v-model="mov.year">
+          <option value="2020" selected>2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+          <option value="2016">2016</option>
+          <option value="2015">2015</option>
+        </select>
         <button @click="addNewMovie()">Add New Movie</button>&nbsp;&nbsp;&nbsp;
         <button @click="resetMovie()">Reset</button>
       </div>
